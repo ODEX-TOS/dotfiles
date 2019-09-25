bfef9b6261fd532a6e87eca497af07bdcce3d5a3
F0xedb <tom@odex.be>
2019-09-25 11:22:51 +0200

    Added a weather module to detect the current temp

--- polybar/scripts/weather.sh
+++ polybar/scripts/weather.sh
@@ -4,8 +4,8 @@ cachedir=~/.cache/tos
 cachefile=${0##*/}-$1
 
 
-cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
 temp=$(curl -s https://en.wttr.in\?0qnT | grep -E '(°C)|(°F)' | awk '{print $(NF-1),$NF}')
+temp=$(printf "%s" "$temp" | grep -E -o '[0-9]* (°C|°F)' --color=none)
 
 if [ ! -d $cachedir ]; then
     mkdir -p $cachedir
@@ -15,6 +15,8 @@ if [ ! -f $cachedir/$cachefile ]; then
     touch $cachedir/$cachefile
     echo "$temp" > "$cachedir"/"$cachefile"
 fi
+
+cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
 
 if [[ "$temp" == *"°C"* ]] || [[ "$temp" == *"°F"* ]] && [[ "$cacheage" -gt 3600 ]]; then
     echo "$temp" > "$cachedir"/"$cachefile"
