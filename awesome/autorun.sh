ec15f3568eef27f43724e875cd586a72ae69f550
F0xedb <tom@odex.be>
2019-10-06 16:16:34 +0200

    Set background based on tos

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -10,4 +10,9 @@ function run {
 
 setxkbmap  $(cat /etc/vconsole.conf | cut -d= -f2 | cut -d- -f1)
 
+nohup tos t s $(cat ~/.config/tos/theme | head -n3 | tail -n1) &> /dev/null &
+nohup tos theme daemon &> /dev/null & # launch a tos daemon
+touchpad.sh &
+
+
 run st
