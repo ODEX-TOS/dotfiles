3308ee38165bddcf089e058950fa3a6f81d2433a
Restyled.io <commits@restyled.io>
2019-10-06 14:40:05 +0000

    Restyled by shellharden

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 function run {
-  if ! pgrep -f $1 ;
+  if ! pgrep -f "$1" ;
   then
-    $@&
+    "$@"&
   fi
 }
 
 
-setxkbmap  $(cat /etc/vconsole.conf | cut -d= -f2 | cut -d- -f1)
+setxkbmap  "$(cat /etc/vconsole.conf | cut -d= -f2 | cut -d- -f1)"
 
-nohup tos t s $(cat ~/.config/tos/theme | head -n3 | tail -n1) &> /dev/null &
+nohup tos t s "$(cat ~/.config/tos/theme | head -n3 | tail -n1)" &> /dev/null &
 nohup tos theme daemon &> /dev/null & # launch a tos daemon
 touchpad.sh &
 
