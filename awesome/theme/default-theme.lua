37548b05c63f4f3cb8876383c2ddece4a9f4e4a6
F0xedb <tom@odex.be>
2019-10-06 16:16:34 +0200

    Set background based on tos

--- awesome/theme/default-theme.lua
+++ awesome/theme/default-theme.lua
@@ -7,6 +7,25 @@ local theme = {}
 theme.icons = theme_dir .. '/icons/'
 theme.font = 'Roboto medium 10'
 
+-- read out files
+-- see if the file exists
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+-- get all lines from a file, returns an empty 
+-- list/table if the file does not exist
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  lines = {}
+  for line in io.lines(file) do 
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
 -- Colors Pallets
 
 -- Custom
@@ -27,7 +46,8 @@ local awesome_overrides =
   --theme.dir             = os.getenv("HOME") .. "/code/awesome-pro/themes/pro-dark"
 
   theme.icons = theme.dir .. '/icons/'
-  theme.wallpaper = '~/Pictures/simple.png'
+  --theme.wallpaper = '~/Pictures/simple.png'
+  theme.wallpaper = lines_from("~/.config/tos/theme")[2]
   --theme.wallpaper = '#e0e0e0'
   theme.font = 'Roboto medium 10'
   theme.title_font = 'Roboto medium 14'
