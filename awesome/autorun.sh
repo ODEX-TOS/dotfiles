afa3f1d24c20ff21cee2da4d41857d442f074e51
Restyled.io <commits@restyled.io>
2019-10-06 14:40:06 +0000

    Restyled by shfmt

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-function run {
-  if ! pgrep -f "$1" ;
-  then
-    "$@"&
+function run() {
+  if ! pgrep -f "$1"; then
+    "$@" &
   fi
 }
 
+setxkbmap "$(cat /etc/vconsole.conf | cut -d= -f2 | cut -d- -f1)"
 
-setxkbmap  "$(cat /etc/vconsole.conf | cut -d= -f2 | cut -d- -f1)"
-
-nohup tos t s "$(cat ~/.config/tos/theme | head -n3 | tail -n1)" &> /dev/null &
-nohup tos theme daemon &> /dev/null & # launch a tos daemon
+nohup tos t s "$(cat ~/.config/tos/theme | head -n3 | tail -n1)" &>/dev/null &
+nohup tos theme daemon &>/dev/null &# launch a tos daemon
 touchpad.sh &
-
 
 run st
