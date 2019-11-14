89bfb7e972ecf27fe29e375ce52e4422e63e98d1
F0xedb <tom@odex.be>
2019-11-14 15:58:56 +0100

    Show updates without using the pacman cache

--- awesome/updater.sh
+++ awesome/updater.sh
@@ -23,14 +23,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-num=0
-unique=$(yay -Sl | grep -E "\[installed: .*\]")
-while read -r line ; do
-        count=$(yay -Sl | grep " $(echo $line | cut -d ' ' -f2) " | wc -l)
-        if [[ "$count" == *"1"* ]]; then
-            num=$(($num+1))
-        fi
-done <<< "$unique"
-echo "$num"
+#num=0
+#unique=$(yay -Sl | grep -E "\[installed: .*\]")
+#while read -r line ; do
+#        count=$(yay -Sl | grep " $(echo $line | cut -d ' ' -f2) " | wc -l)
+#        if [[ "$count" == *"1"* ]]; then
+#            num=$(($num+1))
+#        fi
+#done <<< "$unique"
+#echo "$num"
+echo "$(checkupdates | wc -l)"
 
 
