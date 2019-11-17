9a4469362ea27aa11368ce57796082aa85605f56
F0xedb <tom@odex.be>
2019-11-17 22:28:09 +0100

    Added transparency as an option

--- awesome/theme/PapyElGringo-theme/init.lua
+++ awesome/theme/PapyElGringo-theme/init.lua
@@ -47,16 +47,16 @@ theme.accent =  mat_colors[config["accent"]] or mat_colors.hue_purple
 -- Background
 theme.background = mat_colors[config["background"]] or mat_colors.blue_grey
 
-theme.background.hue_800 = theme.background.hue_800 .. '66' --99
+theme.background.hue_800 = theme.background.hue_800 .. (config["background_transparent"] or '66') --99
 
 if config["background_800"] ~= nil then
-  theme.background.hue_800 = "#" .. config["background_800"] .. '66'
+  theme.background.hue_800 = "#" .. config["background_800"] .. (config["background_transparent"] or '66')
 end
 
-theme.background.hue_900 = theme.background.hue_900 .. '66' -- 121e25
+theme.background.hue_900 = theme.background.hue_900 .. (config["background_transparent"] or '66') -- 121e25
 
 if config["background_900"] ~= nil then
-  theme.background.hue_900 = "#" .. config["background_900"] .. '66'
+  theme.background.hue_900 = "#" .. config["background_900"] ..  (config["background_transparent"] or  '66')
 end
 
 local awesome_overrides = function(theme)
