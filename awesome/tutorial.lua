a36c1df8e5e4f35112b12e72d5c26c1dede6d428
F0xedb <tom@odex.be>
2019-11-29 23:05:28 +0100

    Made tutorial not use deprecated versions

--- awesome/tutorial.lua
+++ awesome/tutorial.lua
@@ -26,40 +26,47 @@
 
 local naughty = require('naughty')
 
-function tutorial ()
-    function workspaces ()
-    naughty.notify({ title = "TOS tutorial!", text = "To go to a new workspace try mod+2, launch a program and switch back with mod+1", timeout = 0, position = "top_left", 
-    destroy = function ()
-        naughty.notify({ title = "TOS tutorial!", text = "For the help menu use mod+f1.", timeout = 0, position = "top_left", 
-        destroy = function ()
-            naughty.notify({ title = "TOS tutorial!", text = "Nice all you need to do now is start the installer, open a terminal (mod+Enter) and type in 'tos c'. Good luck on your journey", timeout = 0, position = "top_left",
-               destroy = function ()
-                  local HOME = os.getenv('HOME')
-                  local FILE = HOME .. '/.cache/tutorial_tos'
-                  io.open(FILE,"w"):write("tutorial is complete"):close()
-               end}) 
-        end})
-    end})
+
+    function secondTip()
+        naughty.notify({ title = "TOS tutorial!", message = "This sets the staking layout of your windows. Notice the pattern.", timeout = 0, position = "bottom_left"}):connect_signal("destroyed", thirdTip)
+    end
+
+    function thirdTip()
+        naughty.notify({ title = "TOS tutorial!", message = "Try to open a few terminals and see what happens. mod+Enter to open a terminal (windows key) Now click on the icon in the bottom left corner a few times", timeout = 0, position = "bottom_left"}):connect_signal("destroyed", fourthTip)
+    end
+
+    function fourthTip()
+        naughty.notify({ title = "TOS tutorial!", message = "To kill a program use mod+q", timeout = 0, position = "top_right"}):connect_signal("destroyed", fifthTip)
+
+    end
+
+    function fifthTip()
+        naughty.notify({ title = "TOS tutorial!", message = "To launch applications use mod+d. Try to start a few and kill them.", timeout = 0, position = "top_right"}):connect_signal("destroyed", sixthTip)
+    end
+
+    function sixthTip()
+        naughty.notify({ title = "TOS tutorial!", message = "Click on the tos logo to access general settings.", timeout = 0, position = "top_left"}):connect_signal("destroyed", seventhTip)
+    end
+
+    function seventhTip()
+        naughty.notify({ title = "TOS tutorial!", message = "To go to a new workspace try mod+2, launch a program and switch back with mod+1", timeout = 0, position = "top_left"}):connect_signal("destroyed", eightTip)
+    end
+
+    function eightTip()
+        naughty.notify({ title = "TOS tutorial!", message = "For the help menu use mod+f1.", timeout = 0, position = "top_left"}):connect_signal("destroyed", ninthTip)
+    end
+
+    function ninthTip()
+        naughty.notify({ title = "TOS tutorial!", message = "Nice all you need to do now is start the installer, open a terminal (mod+Enter) and type in 'tos c'. Good luck on your journey", timeout = 0, position = "top_left"}):connect_signal("destroyed", finish)
+    end
+
+    function finish()
+        local HOME = os.getenv('HOME')
+        local FILE = HOME .. '/.cache/tutorial_tos'
+        io.open(FILE,"w"):write("tutorial is complete"):close()
     end
 
 
-    naughty.notify({ title = "TOS tutorial!", text = "<- All the icons on the left represent workspaces", timeout = 0, position = "top_left", 
-    destroy = function ()
-        naughty.notify({ title = "TOS tutorial!", text = "<- This sets the staking layout of your windows. Notice the pattern.", timeout = 0, position = "bottom_left", 
-        destroy = function ()
-            naughty.notify({ title = "TOS tutorial!", text = "Try to open a few terminals and see what happens. mod+Enter to open a terminal (windows key) Now click on the icon in the bottom left corner a few times", timeout = 0, position = "bottom_left", 
-            destroy = function ()
-                naughty.notify({ title = "TOS tutorial!", text = "To kill a program use mod+q", timeout = 0, position = "top_right", 
-                destroy = function ()
-                    naughty.notify({ title = "TOS tutorial!", text = "To launch applications use mod+d. Try to start a few and kill them.", timeout = 0, position = "top_right", 
-                    destroy = function ()
-                        naughty.notify({ title = "TOS tutorial!", text = "<- Click on the tos logo to access general settings.", timeout = 0, position = "top_left", destroy = workspaces})
-                    end})
-                end})
-            end})
-        end})
-    end})
-end
 
 local HOME = os.getenv('HOME')
 local FILE = HOME .. '/.cache/tutorial_tos'
@@ -67,5 +74,5 @@ local f=io.open(FILE,"r")
 if f~=nil then 
         io.close(f) 
 else 
-    tutorial()
+    naughty.notify({ title = "TOS tutorial!", message = "All the icons on the left represent workspaces", timeout = 0, position = "top_left"}):connect_signal("destroyed", secondTip)
 end
