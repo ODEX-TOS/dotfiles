cb4a2d289c9a4201c0ac374d8fd2002948d3720a
F0xedb <tom@odex.be>
2020-03-13 16:28:30 +0200

    Added a new and improved trash

--- awesome/widget/xdg-folders/trash.lua
+++ awesome/widget/xdg-folders/trash.lua
@@ -22,54 +22,136 @@
 --OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 --SOFTWARE.
 ]]
-
+local wibox = require('wibox')
 local awful = require('awful')
 local naughty = require('naughty')
-local watch = require('awful.widget.watch')
-local wibox = require('wibox')
-local clickable_container = require('widget.material.clickable-container')
 local gears = require('gears')
+
+local clickable_container = require('widget.material.clickable-container')
 local dpi = require('beautiful').xresources.apply_dpi
 
-local PATH_TO_ICONS =  '/etc/xdg/awesome/widget/xdg-folders/icons/'
+local widget_icon_dir = '/etc/xdg/awesome/widget/xdg-folders/icons/'
 
-local trashWidget =
-  wibox.widget {
-  {
-    id = 'icon',
-    widget = wibox.widget.imagebox,
-    resize = true
-  },
-  layout = wibox.layout.align.horizontal
+local trash_widget = wibox.widget {
+	{
+		id = 'trash_icon',
+		image = widget_icon_dir .. 'user-trash-empty' .. '.svg',
+		resize = true,
+		widget = wibox.widget.imagebox
+	},
+	layout = wibox.layout.align.horizontal
 }
 
-local trash_button = clickable_container(wibox.container.margin(trashWidget, dpi(8), dpi(8), dpi(8), dpi(8)))
+local trash_menu = awful.menu({
+	items = {
+		{
+			"Open trash",
+			function() awful.spawn.easy_async_with_shell("gio open trash:///", function(stdout) end, 1) end,
+			widget_icon_dir .. 'open-folder' .. '.svg'
+		},
+		{
+			"Delete forever", 
+			{
+				{
+					'Yes',
+					function() awful.spawn.easy_async_with_shell("gio trash --empty", function(stdout) end, 1) end,
+					widget_icon_dir .. 'yes' .. '.svg'
+				},
+				{
+					'No',
+					'',
+					widget_icon_dir .. 'no' .. '.svg'
+				}
+			},
+			widget_icon_dir .. 'user-trash-empty' .. '.svg'
+		},
+ 
+ }
+})
+
+
+local trash_button = wibox.widget {
+	{
+		trash_widget,
+		margins = dpi(10),
+		widget = wibox.container.margin
+	},
+	widget = clickable_container
+}
+
+-- Tooltip for trash_button
+trash_tooltip = awful.tooltip {
+	objects = {trash_button},
+	mode = 'outside',
+	align = 'right',
+	markup = 'Trash',
+	margin_leftright = dpi(8),
+	margin_topbottom = dpi(8),
+	preferred_positions = {'right', 'left', 'top', 'bottom'}
+}
+
+-- Mouse event for trash_button
 trash_button:buttons(
-  gears.table.join(
-    awful.button(
-      {},
-      1,
-      nil,
-      function()
-        awful.spawn.easy_async_with_shell("xdg-open trash://", function(stderr) end, 1)
-      end
-    )
-  )
+	gears.table.join(
+		awful.button(
+			{},
+			1,
+			nil,
+			function()
+				awful.spawn({'gio', 'open', 'trash:///'}, false)
+			end
+		),
+		awful.button(
+			{},
+			3,
+			nil,
+			function()
+				trash_menu:toggle() 
+				trash_tooltip.visible = not trash_tooltip.visible
+			end
+		)
+	)
 )
 
--- Alternative to naughty.notify - tooltip. You can compare both and choose the preferred one
-awful.tooltip(
-  {
-    objects = {trash_button},
-    mode = 'outside',
-    align = 'right',
-    timer_function = function()
-      return 'Trash'
-    end,
-    preferred_positions = {'right', 'left', 'top', 'bottom'}
-  }
-)
 
-trashWidget.icon:set_image(PATH_TO_ICONS .. 'user-trash-full' .. '.svg')
+
+-- Update icon on changes
+local check_trash_list = function()
+	awful.spawn.easy_async_with_shell('gio list trash:/// | wc -l', function(stdout) 
+		if tonumber(stdout) > 0 then
+			trash_widget.trash_icon:set_image(widget_icon_dir .. 'user-trash-full' .. '.svg')
+
+			awful.spawn.easy_async_with_shell('gio list trash:///', function(stdout) 
+
+				trash_tooltip.markup = '<b>Trash contains:</b>\n' .. stdout:gsub('\n$', '')
+
+			end, false)
+
+
+		else
+			trash_widget.trash_icon:set_image(widget_icon_dir .. 'user-trash-empty' .. '.svg')
+
+			trash_tooltip.markup = 'Trash empty'
+
+		end
+	end, false)
+end
+
+
+-- Check trash on awesome (re)-start
+check_trash_list()
+
+
+-- Kill the old process of gio monitor trash:///
+awful.spawn.easy_async_with_shell('ps x | grep \'gio monitor trash:///\' | grep -v grep | awk \'{print  $1}\' | xargs kill', function() 
+
+	awful.spawn.with_line_callback('gio monitor trash:///', {
+		stdout = function(_)
+			check_trash_list()
+		end
+	})
+
+end, false)
+
 
 return trash_button
